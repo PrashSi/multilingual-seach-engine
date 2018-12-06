759c59b48aa467b4abc9643477b03a596c6c06c4
James Huang <jameshuang@Jamess-MacBook-Pro.local>
2018-12-06 09:56:24 -0500

    updated html

---
+++ searchUI/static/js/script.js
@@ -0,0 +1,13 @@
+$(".dropdown-menu li a").click(function(){
+var selText = $(this).text();
+$(this).parents('.btn-group').find('.dropdown-toggle').html(selText+' <span class="caret"></span>');
+});
+
+$('#myTab a').click(function (e) {
+    e.preventDefault();
+    $(this).tab('show');
+});
+
+$(function () {
+$('#myTab a:first').tab('show');
+});
